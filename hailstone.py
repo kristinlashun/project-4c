5251feceec68db3c12dee469f7f8bd56b55c9c02
Kristin Evans <kristinlashun@gmail.com>
2023-10-25 22:17:57 -0700

    no recursion fixed

--- hailstone.py
+++ hailstone.py
@@ -1,13 +1,17 @@
-def hailstone(n): 
-  
-    # Base case 
-    if n == 1: 
-        return 1
-  
-    # If n is even 
-    if n % 2 == 0: 
-        return hailstone(n // 2) 
-  
-    # If n is odd 
-    else: 
-        return hailstone(3 * n + 1)
+def hailstone(n):
+  # Initialize a counter to keep track of steps
+  steps = 0
+
+  # Loop until n becomes 1
+  while n != 1:
+      # If n is even
+      if n % 2 == 0:
+          n = n // 2
+      # If n is odd
+      else:
+          n = 3 * n + 1
+
+      
+      steps += 1
+
+  return steps
